469485e4497eeec26e16038c0fde8716e139fb8c
Nzola Kiampava <nzolakiampava@gmail.com>
2023-06-22 16:26:56 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import React from 'react'
+import HomeScreen from './src/screens/HomeScreen'
+
+
+const App = () => {
+  return (
+    <View>
+      <HomeScreen />
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
