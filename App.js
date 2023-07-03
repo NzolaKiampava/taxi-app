a98a5f5a67ab74aa647ccafcb81a9d4dbf2d556b
Nzola Kiampava <nzolakiampava@gmail.com>
2023-07-03 15:22:02 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
+import 'react-native-gesture-handler';
+import { ENV_VARIABLE_NAME } from "@env"
 import { StyleSheet, Text, View, Dimensions } from 'react-native'
 import React from 'react'
-import HomeScreen from './src/screens/HomeScreen'
+import RoutNavigator from './src/navigations/RoutNavigator'
+import HomeScreen from './src/screens/HomeScreen';
+import { OriginContextProvider } from './src/contexts/contexts';
 
 
 const App = () => {
   return (
-    <View>
-      <HomeScreen />
-    </View>
+      <OriginContextProvider>
+        <RoutNavigator />
+      </OriginContextProvider>
   )
 }
 
