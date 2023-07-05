f83077fda3e2195234b518d4cf2c70d9f4ee1c72
Nzola Kiampava <nzolakiampava@gmail.com>
2023-07-05 16:19:19 +0100

    Update MapComponent.js

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,138 +1,167 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, View, Image } from 'react-native'
+import React, { Component } from "react";
+import { Text, StyleSheet, View, Image } from "react-native";
 import { colors, parameters } from "../global/styles";
 import { mapStyle } from "../global/mapStyle";
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 
 export default class MapComponent extends Component {
+
+  constructor(){
+    super()
+      this.state = {
+
+      }
+      this._map = React.createRef(35)
+  }
+
+  componentDidUpdate(){
+    setInterval(()=>{
+      if(this.props.userDestination.latitude !== null){
+        this._map.current.fitToCoordinates(
+          [this.props.userOrigin, this.props.userDestination],{
+            edgePadding:{top:450, right:50, left:50, bottom:350},
+            animated:true
+          }
+        )
+      }
+    }, 500)
+  }
+
   render() {
     return (
-        <View>
-            <MapView
-              provider={PROVIDER_GOOGLE}
-              style={styles.map}
-              customMapStyle={mapStyle}
-              
+      <View>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          customMapStyle={mapStyle}
+          ref={this._map}
+        >
+          {this.props.userOrigin.latitude != null && (
+            <Marker
+              coordinate={this.props.userOrigin}
+              anchor={{ x: 0.5, y: 0.5 }}
             >
-              { this.props.userOrigin.latitude != null &&
-                <Marker coordinate={this.props.userOrigin} anchor={{x:0.5,y:0.5}}>
-                  <Image 
-                    source={require('../../assets/location.png')}
-                    style={styles.markerOrigin2}
-                    resizeMode='cover'
-                  />
-                </Marker>
-              }
-              
-            </MapView>
-        </View>
-    )
+              <Image
+                source={require("../../assets/location.png")}
+                style={styles.markerOrigin2}
+                resizeMode="cover"
+              />
+            </Marker>
+          )}
+
+          {this.props.userDestination.latitude != null && (
+            <Marker
+              coordinate={this.props.userDestination}
+              anchor={{ x: 0.5, y: 0.5 }}
+            >
+              <Image
+                source={require("../../assets/location.png")}
+                style={styles.markerDestination}
+                resizeMode="cover"
+              />
+            </Marker>
+          )}
+        </MapView>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   map: {
-    height:"100%",
-     width:"100%"
-    },
+    height: "100%",
+    width: "100%",
+  },
 
-    
-      markerWrapOrigin: {
-       //  alignItems: "center",
-        // justifyContent: "center",
-          width:40,
-         height:20,
-        // marginTop:0
-        },
-        markerOrigin: {
-           width: 16,
-           height: 16,
-           borderRadius:8
-        },
-  
-        destination: {
-           width:20,
-          height:20,
-          backgroundColor:colors.black,
-          alignItems:"center",
-          justifyContent:"center"
-         },
+  markerWrapOrigin: {
+    //  alignItems: "center",
+    // justifyContent: "center",
+    width: 40,
+    height: 20,
+    // marginTop:0
+  },
+  markerOrigin: {
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+  },
 
-         view1: {
-           width:7,
-          height:7,
-          backgroundColor:colors.white
-         },
-         markerDestination: {
-          width: 16,
-          height: 16,
-          
-         },
-         
-         markerOrigin2: {
-           width: 20,
-           height:20,
-           borderRadius:10
-          },
+  destination: {
+    width: 20,
+    height: 20,
+    backgroundColor: colors.black,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
-   car:{
-       paddingTop:0,
-       width: 40,
-       height: 20,
-      },
+  view1: {
+    width: 7,
+    height: 7,
+    backgroundColor: colors.white,
+  },
+  markerDestination: {
+    width: 16,
+    height: 16,
+  },
 
-      view2:{
-       position:"absolute",
-       top:10,
-       right:12,
-       backgroundColor:colors.white,
-       height:40,
-       width:180,
-       borderRadius:20,
-       justifyContent:"center",
-       alignItems:"center",
-       marginTop:2, 
-       zIndex: 8
-       
-     },    
+  markerOrigin2: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+  },
 
-view3:{ flexDirection:"row",
-alignItems:"center",
-//marginRight:15,
-//backgroundColor:"white",
-//paddingHorizontal:2,
-paddingVertical:2,
-//borderRadius:20
-},
+  car: {
+    paddingTop: 0,
+    width: 40,
+    height: 20,
+  },
 
-view4:{
-   position:"absolute",
-   top:50,
-   left:12,
-   backgroundColor:colors.white,
-   height:40,
-   width:140,
-   borderRadius:20,
-   justifyContent:"center",
-   alignItems:"center",
-   marginTop:2, 
-   zIndex: 8
-   
- }, 
+  view2: {
+    position: "absolute",
+    top: 10,
+    right: 12,
+    backgroundColor: colors.white,
+    height: 40,
+    width: 180,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 2,
+    zIndex: 8,
+  },
 
- location: {
-   width: 20,
-   height: 20,
-   borderRadius:9,
-   backgroundColor:colors.black,
-   alignItems:"center",
-   justifyContent:"center"
-   
-   }, 
-   
-view9:{width:6,
- height:6,
- borderRadius:4,
- backgroundColor:"white"
-}     
-})
+  view3: {
+    flexDirection: "row",
+    alignItems: "center",
+    //marginRight:15,
+    //backgroundColor:"white",
+    //paddingHorizontal:2,
+    paddingVertical: 2,
+    //borderRadius:20
+  },
+
+  view4: {
+    position: "absolute",
+    top: 50,
+    left: 12,
+    backgroundColor: colors.white,
+    height: 40,
+    width: 140,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 2,
+    zIndex: 8,
+  },
+
+  location: {
+    width: 20,
+    height: 20,
+    borderRadius: 9,
+    backgroundColor: colors.black,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  view9: { width: 6, height: 6, borderRadius: 4, backgroundColor: "white" },
+});
